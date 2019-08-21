22fbe2e862576ba5a9cd5240f3286944215b15fa
bonnina <37539513+bonnina@users.noreply.github.com>
2019-08-21 08:10:53 +0300

    Move common script to wwwroot

--- ATM lab/wwwroot/js/site.js
+++ ATM lab/wwwroot/js/site.js
@@ -1,4 +1,10 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function onClear() {
+    $("#number").text('');
+}
 
-// Write your JavaScript code.
+function onKeyPress(value) {
+    var cardNumber = $("#number").text();
+    cardNumber += value;
+
+    $("#number").text(cardNumber);
+}
