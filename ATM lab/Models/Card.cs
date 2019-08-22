0fc4306ab819e119f10087f48a357063c3da36d5
bonnina <37539513+bonnina@users.noreply.github.com>
2019-08-22 23:42:29 +0300

    Add card model

---
+++ ATM lab/Models/Card.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace ATM_lab.Models
+{
+    public class Card
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [RegularExpression(@"\d{16}", ErrorMessage = "Card number should be 16 digits")]
+        public string CardNumber { get; set; }
+
+        [Required]
+        [RegularExpression(@"\d{4}", ErrorMessage = "PIN should be 4 digits")]
+        public string PIN { get; set; }
+
+        [Required]
+        public bool Blocked { get; set; }
+    }
+}
