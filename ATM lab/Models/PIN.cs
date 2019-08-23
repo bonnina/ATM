0a5ab5486a229fd09cb737cab2ff08aa42c3f25d
bonnina <37539513+bonnina@users.noreply.github.com>
2019-08-23 22:42:22 +0300

    Add PIN model

---
+++ ATM lab/Models/PIN.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ATM_lab.Models
+{
+    public class PIN
+    {
+        [Required]
+        [StringLength(4)]
+        public string Code { get; set; }
+
+        [Required]
+        public int AttemptNum { get; set; }
+    }
+}
