f06537cb16e15eb1dd350965d887c2e6ac12a638
bonnina <37539513+bonnina@users.noreply.github.com>
2019-09-13 22:55:58 +0300

    Initial migration

---
+++ ATM lab/Migrations/20190913194717_Initial.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ATM_lab.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Card",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CardNumber = table.Column<string>(nullable: false),
+                    PIN = table.Column<string>(maxLength: 4, nullable: false),
+                    FailedLogins = table.Column<int>(nullable: false),
+                    Blocked = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Card", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Card");
+        }
+    }
+}
