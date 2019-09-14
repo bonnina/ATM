3acb87011bfe5547729edae438e68417b53b45ed
bonnina <37539513+bonnina@users.noreply.github.com>
2019-09-14 23:18:46 +0300

    Add Seed initial data

---
+++ ATM lab/Models/Seed.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ATM_lab.Models
+{
+    public class Seed
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new ATMcontext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<ATMcontext>>()))
+            {
+                if (context.Card.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Card.AddRange(
+                    new Card
+                    {
+                        CardNumber = "5555444433332222",
+                        PIN = "5432",
+                        FailedLogins = 0,
+                        Blocked = false
+                    },
+                    new Card
+                    {
+                        CardNumber = "4444333322221111",
+                        PIN = "4321",
+                        FailedLogins = 0,
+                        Blocked = false
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
