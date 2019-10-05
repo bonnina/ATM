cb13a4bb2b80217bb748e52d110cd6acafdcaddc
bonnina <37539513+bonnina@users.noreply.github.com>
2019-10-06 00:00:02 +0300

    Add Error model

---
+++ ATM lab/Models/Error.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ATM_lab.Models
+{
+    public class Error
+    {
+        public string Message { get; set; }
+
+        public string PrevUrl { get; set; }
+    }
+}
