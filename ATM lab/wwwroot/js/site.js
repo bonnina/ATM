8cc471e82a0896ec68a3c2526896ac35efa20e65
bonnina <37539513+bonnina@users.noreply.github.com>
2019-10-27 21:50:19 +0200

    Update functions to manipulate input value

--- ATM lab/wwwroot/js/site.js
+++ ATM lab/wwwroot/js/site.js
@@ -1,14 +1,11 @@
-﻿function onClear() {
-    $("#number").text('');
+﻿function onKeyPress(value) {
+    var input = $("#inputNumber")[0];
+
+    input.value += value;
 }
 
-function onKeyPress(value) {
- //   var cardNumber = $("#number").text();
- //   cardNumber += value;
+function onClear() {
+    var input = $("#inputNumber")[0];
 
- //   $("#number").text(cardNumber);
-
-    var screen = $("#screen")[0];
-    var input = screen.firstChild;
-    input.value += value;
+    input.value = "";
 }
