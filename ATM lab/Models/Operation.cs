2a3064af5ed43aae8b67cb1023ec021b6d6026c4
bonnina <37539513+bonnina@users.noreply.github.com>
2019-11-05 23:41:46 +0200

    Add Operation model

---
+++ ATM lab/Models/Operation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace ATM_lab.Models
+{
+    public class Operation
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string CardNumber { get; set; }
+
+        [Required]
+        public int Amount { get; set; }
+
+        [Required]
+        public DateTime Timestamp { get; set; }
+    }
+}
