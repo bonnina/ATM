79084dc227b805d01635bbee51d60f902fbc85ee
bonnina <37539513+bonnina@users.noreply.github.com>
2019-11-08 07:24:19 +0200

    Add Operation controller

---
+++ ATM lab/Controllers/OperationController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ATM_lab.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ATM_lab.Controllers
+{
+    public class OperationController : Controller
+    {
+        private readonly ATMcontext _context;
+
+        public OperationController(ATMcontext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Index(string cardNumber)
+        {
+            Card card = new Card
+            {
+                CardNumber = cardNumber
+            };
+
+            return View(card);
+        }
+    }
+}
