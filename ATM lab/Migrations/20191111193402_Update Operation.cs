f74d18bfbaf49154c18a0d41e5ac71521c1dd121
bonnina <37539513+bonnina@users.noreply.github.com>
2019-11-11 21:35:23 +0200

    Add migration (Update operation)

---
+++ ATM lab/Migrations/20191111193402_Update Operation.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ATM_lab.Migrations
+{
+    public partial class UpdateOperation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Type",
+                table: "Operation",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "Operation");
+        }
+    }
+}
