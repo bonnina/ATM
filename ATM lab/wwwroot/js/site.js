24e4108be8940bb803d8280ce1e6a1b6e97ad845
bonnina <37539513+bonnina@users.noreply.github.com>
2019-11-21 00:12:02 +0200

    update functions

--- ATM lab/wwwroot/js/site.js
+++ ATM lab/wwwroot/js/site.js
@@ -1,7 +1,18 @@
-﻿function onKeyPress(value) {
+﻿var pin;
+
+function isPinView() {
+    var url = window.location.pathname;
+
+    return url == "/Home/Pin";
+}
+
+function onKeyPress(value) {
     var input = $("#inputNumber")[0];
 
-    input.value += value;
+    if (isPinView) {
+        pin += value;
+        input.value += '*';
+    }
 }
 
 function onClear() {
