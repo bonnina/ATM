dd86e11155388b017ccb7d6b1364aae507abb261
bonnina <37539513+bonnina@users.noreply.github.com>
2019-11-22 08:43:10 +0200

    update functions for Pin

--- ATM lab/wwwroot/js/site.js
+++ ATM lab/wwwroot/js/site.js
@@ -1,4 +1,18 @@
 ﻿var pin;
+var backgroundColor = "#a3a375";
+
+function getInputNode() {
+    return $("#inputNumber")[0];
+}
+
+function submitPin() {
+    var input = getInputNode();
+
+    input.style.color = backgroundColor; 
+    input.value = pin;
+
+    $("#Ok")[0].click();
+}
 
 function isPinView() {
     var url = window.location.pathname;
@@ -7,11 +21,14 @@ function isPinView() {
 }
 
 function onKeyPress(value) {
-    var input = $("#inputNumber")[0];
+    var input = getInputNode();
 
     if (isPinView) {
         pin += value;
         input.value += '*';
+
+        if (pin.length === 4)
+            submitPin()
     }
 }
 
