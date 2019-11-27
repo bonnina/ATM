b2f086689a2b6ed77be1da70e1e7c566462c7836
bonnina <37539513+bonnina@users.noreply.github.com>
2019-11-27 09:33:14 +0200

    Migration (models' updates)

---
+++ ATM lab/Migrations/20191127072842_Update Card and Operation to use decimals.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ATM_lab.Migrations
+{
+    public partial class UpdateCardandOperationtousedecimals : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Amount",
+                table: "Operation",
+                nullable: false,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Balance",
+                table: "Cards",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Balance",
+                table: "Cards");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Amount",
+                table: "Operation",
+                nullable: false,
+                oldClrType: typeof(decimal));
+        }
+    }
+}
