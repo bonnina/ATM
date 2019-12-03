84684bed9dda161e2ee6abb53b620101a86bbf06
bonnina <37539513+bonnina@users.noreply.github.com>
2019-12-03 15:10:54 +0200

    Add consts (operation types)

---
+++ ATM lab/Constants/OperationType.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ATM_lab.Constants
+{
+    public static class OperationType
+    {
+        public static string BALANCE = "Balance";
+        public static string WITHDRAWAL = "Withdrawal";
+    }
+}
